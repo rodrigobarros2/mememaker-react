ef90c101472d8707352fac5679db4adb3a494f1e
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-08-15 02:05:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
+import Home from './pages/Home';
 
 function App() {
 	return (
-		<div>
-			<h1>lol</h1>
-		</div>
+		<>
+			<GlobalStyles />
+			<Home />
+		</>
 	);
 }
 
